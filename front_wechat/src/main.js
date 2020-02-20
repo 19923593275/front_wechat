4326883a3c99583ac59374c7aa37bdd381ab7c80
19923593275 <1143022649@qq.com>
2020-02-20 17:41:14 +0800

    升级vant框架版本

--- front_wechat/src/main.js
+++ front_wechat/src/main.js
@@ -5,6 +5,8 @@ import store from './store/index.js'
 import api from './request/api'
 import '@/permission'
 import wechatAuth from './plugins/wechatAuth'
+import 'amfe-flexible/index'
+import '@/assets/css/iconfont.css'
 
 Vue.use(wechatAuth, {
   appid: process.env.VUE_APP_WECHAT_APPID
@@ -13,8 +15,9 @@ Vue.prototype.$api = api
 Vue.config.productionTip = false
 
 
-import { Button, Tabbar, TabbarItem, Icon, Swipe, SwipeItem, Lazyload, Toast} from 'vant'
-Vue.use(Button).use(Tabbar).use(TabbarItem).use(Icon).use(Swipe).use(SwipeItem).use(Lazyload).use(Toast);
+import { Button, Tabbar, TabbarItem, Icon, Swipe, SwipeItem, Lazyload, Toast, Col, Row, Image, Form, Field, Cell, CellGroup} from 'vant';
+Vue.use(Button).use(Tabbar).use(TabbarItem).use(Icon).use(Swipe).use(SwipeItem).use(Lazyload).use(Toast).use(Col).use(Row).use(Image)
+.use(Form).use(Field).use(Cell).use(CellGroup);
 
 new Vue({
   router,
