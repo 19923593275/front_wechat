99b39d9faacd72b3da211991319ba4a9654ccf90
19923593275 <1143022649@qq.com>
2020-02-27 23:52:08 +0800

    新增jquery

--- front_wechat/vue.config.js
+++ front_wechat/vue.config.js
@@ -1,32 +1,43 @@
 const autoprefixer = require('autoprefixer');
 const pxtorem = require('postcss-pxtorem');
+const webpack = require('webpack');
+const path = require('path');
 module.exports = {
-    devServer: {
-        proxy: {
-            '/api': {
-                target: process.env.VUE_APP_BASE_API,
-                changeOrigin: true,
-                ws: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
-    },
-    css:{
-        // modules:false,
-        // extract:true,
-        sourceMap:false,
-        loaderOptions: {
-          postcss: {
-            plugins: [
-              autoprefixer(),
-              pxtorem({
-                rootValue: 37.5,
-                propList: ['*']
-              })
-            ]
+  devServer: {
+      proxy: {
+          '/api': {
+              target: process.env.VUE_APP_BASE_API,
+              changeOrigin: true,
+              ws: true,
+              pathRewrite: {
+                  '^/api': ''
+              }
           }
-        }
       }
+  },
+  css:{
+    // modules:false,
+    // extract:true,
+    sourceMap:false,
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          autoprefixer(),
+          pxtorem({
+            rootValue: 37.5,
+            propList: ['*']
+          })
+        ]
+      }
+    }
+  },
+  configureWebpack: {//引入jquery
+    plugins: [
+      new webpack.ProvidePlugin({
+        $:"jquery",
+        jQuery:"jquery",
+        "windows.jQuery":"jquery"
+      })
+    ]
+  }
 }
